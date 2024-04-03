379833c4755e68471d85327709bc93fad652a568
tbehrenso <thomas.behrens@student.unibe.ch>
2024-04-03 12:31:11 +0200

    methylKit now takes multiple files to process

--- methylKit.R
+++ methylKit.R
@@ -3,19 +3,26 @@ setwd('C:/Users/tbehr/Desktop/UU/WD')
 library('methylKit')
 library('ggplot2')
 
-IVPF15_obj <- methRead('./IVPF15_MethylExt_Ign10/bedGraph/IVPF15_bismark.cov.gz', sample.id='IVPF15', assembly='UCD1.3', pipeline='bismarkCoverage')
+file_list <- list(
+  './IVPF15_MethylExt_Ign10/bedGraph/IVPF15_bismark.cov.gz',
+  './IVPF11_MethylExt_bedGraph/bedGraph/IVPF11_T1D_1_bismark_bt2_pe.bismark.cov.gz'
+)
 
-getMethylationStats(IVPF15_obj, both.strands=F, plot=T)
-getMethylationStats(IVPF15_obj, both.strands=F, plot=F)
+IVPF_obj <- methRead(file_list, sample.id=list('IVPF15','IVPF11'), assembly='UCD1.3', treatment=c(0,0), pipeline='bismarkCoverage')
 
+getMethylationStats(IVPF_obj[[1]], both.strands=F, plot=T)
+getMethylationStats(IVPF_obj[[1]], both.strands=F, plot=F)
 
-getCoverageStats(IVPF15_obj, both.strands=F, plot=T)
-getCoverageStats(IVPF15_obj, both.strands=F, plot=F)
+getCoverageStats(IVPF_obj[[1]], both.strands=F, plot=T)
+getCoverageStats(IVPF_obj[[1]], both.strands=F, plot=F)
 
 # filter by coverage
 ## NOTE: never seems to be below 10. Has this filtering been done before?
-IVPF_obj_filt <- filterByCoverage(IVPF15_obj, lo.count=10)
+IVPF_obj_filt <- filterByCoverage(IVPF_obj, lo.count=10)
 
 # Normalization
 ## (if multiple samples)
+IVPF_obj_norm <- normalizeCoverage(IVPF_obj_filt, method = "median")  # can also use 'mean' method. Should investigate differences
 
+# Merge Data
+IVPF_merged <- unite(IVPF_obj_norm)
